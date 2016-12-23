20e92e402f199a67faf8469bd59ed0d54553c36c
panjichn <panji@detik.com>
2016-12-23 15:28:21 +0700

    Dipisah dan ganti php
    Dipisah dan ganti php

---
+++ footer.php
@@ -0,0 +1,79 @@
+        <!-- Section Start - Footer -->
+        <section class='footer bg-black padding-top-75 padding-bottom-25 '>
+            <!-- Angled Section - Start -->
+            <div class="angled_down_inside black">
+                <div class="slope upleft"></div>
+                <div class="slope upright"></div>
+            </div>
+            <!-- Angled Section - End -->
+            <div class="container">
+                <div class="row">
+                    <!-- Text Widget - Start -->
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-widget inviewport animated delay1" data-effect="fadeInUp">
+                        <div class="logo">
+                            <span><img src="img/logo-flat.png" class="img-responsive" width="55%"></span>
+                        </div>
+                        <p>This is a unique & creatively designed layout built in Photoshop with a modern look. All the files are well organized and named as per content. Its very easy to change any part.</p>
+                        <p>Email: youremail@example.com <br> Phone: +1 234 5678 910</p>
+                    </div>
+                    <!-- Text Widget - End -->
+                    <!-- Twitter Widget - Start -->
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 twitter-widget inviewport animated delay2" data-effect="fadeInUp">
+                        <h4>Twitter</h4>
+                        <div class="headul left-align"></div>
+                        <div class="tweet">
+                            <i class="mdi mdi-twitter"></i>
+                            <div class="message"><strong>@thePixelsTheme</strong> Actually preparing a blog :)  <small>10 minutes ago</small></div>
+                        </div>
+                        <div class="tweet">
+                            <i class="mdi mdi-twitter"></i>
+                            <div class="message">Inspired by <strong>@mattjohn</strong> #10Bootstrap #vime <small>30 minutes ago</small></div>
+                        </div>
+                        <div class="tweet">
+                            <i class="mdi mdi-twitter"></i>
+                            <div class="message"><strong>@AmazingDesign</strong> Current design trends of a blog <small>1 hour ago</small></div>
+                        </div>
+                    </div>
+                    <!-- Twitter Widget - End -->
+                    <!-- Flickr Widget - Start -->
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 flickr-widget inviewport animated delay3" data-effect="fadeInUp">
+                        <h4>Flickr</h4>
+                        <div class="headul left-align"></div>
+                        <div class="row images">
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-1.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-2.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-3.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-4.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-5.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 col-sm-2 col-xs-4"><img alt="flickr-image" src="img/flickr-6.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 hidden-sm hidden-xs"><img alt="flickr-image" src="img/flickr-7.jpg" class="img-responsive"></div>
+                            <div class="col-md-3 hidden-sm hidden-xs"><img alt="flickr-image" src="img/flickr-8.jpg" class="img-responsive"></div>
+                        </div>
+                    </div>
+                    <!-- Flickr Widget - End -->
+                </div>
+            </div>
+            <!-- Copyright Bar - Start -->
+            <div class="copyright">
+                <div class="col-md-12">
+                    <div class="container">
+                        <div class="">
+                            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 message inviewport animated delay1" data-effect="fadeInUp">
+                                <span class="">Copyright &copy; 2017 detikcom, All right reserved</span>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-12 social">
+                                <a href="#" class="inviewport animated delay1" data-effect="fadeInUp"><i class="mdi mdi-facebook text-on-primary"></i></a>
+                                <a href="#" class="inviewport animated delay2" data-effect="fadeInUp"><i class="mdi mdi-twitter text-on-primary"></i></a>
+                                <a href="#" class="inviewport animated delay3" data-effect="fadeInUp"><i class="mdi mdi-dribbble text-on-primary"></i></a>
+                                <a href="#" class="inviewport animated delay4" data-effect="fadeInUp"><i class="mdi mdi-google-plus text-on-primary"></i></a>
+                                <a href="#" class="inviewport animated delay5" data-effect="fadeInUp"><i class="mdi mdi-youtube-play text-on-primary"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Copyright Bar - End -->
+        </section>
+        <!-- Section End - Footer -->
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,203 @@
+    <body class=" angled  yellow">
+        <!-- Section Start - Header -->
+        <section class="header bg-lightgray header-2" >
+            <!-- Menu Top Bar - Start -->
+            <div class="topbar " data-effect="fadeIn">
+                <div class="menu">
+                    <div class="primary inviewport animated delay4" data-effect="fadeInRightBig">
+                        <div class='cssmenu'>
+                            <!-- Menu - Start -->
+                            <ul class='menu-ul'><li class='has-sub'>
+                                <a href='#'>Home <i class='mdi mdi-chevron-down'></i></a>
+                                <ul>
+                                    <li>
+                                        <a href='index.html'>Home 1</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-2.html'>Home 2</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-3.html'>Home 3</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-parallax.html'>Parallax One Page</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-personal.html'>Personal</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-slider.html'>Slider Header</a>
+                                    </li>
+                                    <li>
+                                        <a href='home-classic.html'>Horizontal Style</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class='has-sub'>
+                                <a href='#'>Pages <i class='mdi mdi-chevron-down'></i></a>
+                                <ul>
+                                    <li>
+                                        <a href='services.html'>Services</a>
+                                    </li>
+                                    <li>
+                                        <a href='team.html'>Our Team</a>
+                                    </li>
+                                    <li>
+                                        <a href='art-of-work.html'>Art of Work</a>
+                                    </li>
+                                    <li>
+                                        <a href='capabilities.html'>Our Capabilities</a>
+                                    </li>
+                                    <li>
+                                        <a href='features.html'>Features</a>
+                                    </li>
+                                    <li>
+                                        <a href='pricing.html'>Pricing Plans</a>
+                                    </li>
+                                    <li>
+                                        <a href='left-sidebar.html'>Left Sidebar</a>
+                                    </li>
+                                    <li>
+                                        <a href='right-sidebar.html'>Right Sidebar</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class='has-sub'>
+                                <a href='#'>About <i class='mdi mdi-chevron-down'></i></a>
+                                <ul>
+                                    <li>
+                                        <a href='about-us-1.html'>About Us 1</a>
+                                    </li>
+                                    <li>
+                                        <a href='about-us-2.html'>About Us 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class='has-sub'>
+                                <a href='#'>Portfolio <i class='mdi mdi-chevron-down'></i></a>
+                                <ul>
+                                    <li>
+                                        <a href='portfolio-grid.html'>Grid</a>
+                                    </li>
+                                    <li>
+                                        <a href='portfolio-masonary.html'>Masonary</a>
+                                    </li>
+                                    <li>
+                                        <a href='portfolio-full.html'>Full Page Width</a>
+                                    </li>
+                                    <li>
+                                        <a href='portfolio-grid-2.html'>Grid (2 Column)</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <!-- Menu - End -->
+                    </div>
+                </div>
+                <div class="black inviewport animated delay4" data-effect="fadeInLeftBig">
+                    <div class='cssmenu'>
+                        <!-- Menu - Start -->
+                        <ul class='menu-ul'><li class='has-sub'>
+                            <a href='#'>Blog <i class='mdi mdi-chevron-down'></i></a>
+                            <ul>
+                                <li>
+                                    <a href='blogs-alternate.html'>Medium Alternate</a>
+                                </li>
+                                <li>
+                                    <a href='blogs-small.html'>Small</a>
+                                </li>
+                                <li>
+                                    <a href='blogs-centered.html'>Centered</a>
+                                </li>
+                                <li>
+                                    <a href='blog-single.html'>Single page</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class='has-sub'>
+                            <a href='#'>Shortcodes <i class='mdi mdi-chevron-down'></i></a>
+                            <ul>
+                                <li>
+                                    <a href='typography.html'>Typography</a>
+                                </li>
+                                <li>
+                                    <a href='angled-sections.html'>Angled Sections</a>
+                                </li>
+                                <li>
+                                    <a href='material-icons.html'>Material Icons</a>
+                                </li>
+                                <li>
+                                    <a href='fontawesome.html'>FontAwesome Icons</a>
+                                </li>
+                                <li>
+                                    <a href='animations.html'>Animations</a>
+                                </li>
+                                <li>
+                                    <a href='media.html'>Embed Media</a>
+                                </li>
+                                <li>
+                                    <a href='column-grid.html'>Grid Columns</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class='has-sub'>
+                            <a href='#'>Shop <i class='mdi mdi-chevron-down'></i></a>
+                            <ul>
+                                <li>
+                                    <a href='products.html'>Products</a>
+                                </li>
+                                <li>
+                                    <a href='product-single.html'>Single Page</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href='contact.html'>Contact</a>
+                        </li>
+                    </ul>
+                    <!-- Menu - End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Menu Top Bar - End -->
+    <!-- Logo and Mobile Menu - Start -->
+    <div class='header-logo-wrap'>
+        <div class="container">
+            <div class="logo col-xs-2">
+                <span><img src="img/logo-flat.png" style="max-width:150px;"></span>
+            </div>
+            <div class="menu-mobile col-xs-10 pull-right cssmenu">
+                <i class="mdi mdi-menu menu-toggle"></i>
+                <ul class="menu" id='parallax-mobile-menu'>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Logo and Mobile Menu - End -->
+    <!-- Header Slide - Start -->
+    <div class="header-slide" style="position:relative;">
+        <img alt="header-banner-image" src="img/banner-2.jpg" class='header-img' style=''>
+        <div class="overlay overlay2">
+            <div class="black inviewport animated delay4" data-effect="fadeInLeftOpacity"></div>
+            <div class="primary inviewport animated delay4" data-effect="fadeInRightOpacity"></div>
+            <!-- Header Text - Start -->
+            <div class="maintext">
+                <div class="primary-text inviewport animated delay4" data-effect="fadeInRightBig">
+                    <div class="left-line">
+                        <h4>Blogger</h4>
+                        <h2>Competition</h2>
+                    </div>
+                </div>
+                <div class="black-text inviewport animated delay4" data-effect="fadeInLeftBig">
+                    <div>
+                        <!--<h1>Competition</h1>-->
+                    </div>
+                </div>
+            </div>
+            <!-- Header Text - End -->
+        </div>
+    </div>
+    <!-- Header Slide - End -->
+</section>
+<!-- Section End - Header -->
